aa851e106e579d21ea58bd5841e1185e5f6e4014
pmihaly <papp.misi@protonmail.com>
2020-03-26 09:38:08 +0100

    Ha nincs token, akkor is írassunk ki hibaüzenetet

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  const data = jwt.verify(token, process.env.JWT_KEY);
   try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const data = jwt.verify(token, process.env.JWT_KEY);
     const { __id } = data;
     const user = await User.findOne({ _id: __id, 'tokens.token': token });
     if (!user) throw new Error();
